0298768c6d1c6f80e66aa8bfd392213d7bc334c8
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2023-11-30 10:34:59 -0600

    added function and worked on script

--- app.js
+++ app.js
@@ -1,68 +1,52 @@
 const app = Vue.createApp({
 
-    data() {
-        return {
-            current: '',
-            previous: [],
-            operator: [],
-            operatorClicked: false,
+     data() {
+          return {
+               current: '10',
+               operator: '',
+               
+          }
+     },
 
-           
-        }
-   },
-
-   method: {
-         clear() {
-              this.current = '';
-              this.previous = [];
-              this.operator = [];
-         },
-    
-         append(number) {
-              if(this.operatorClicked) {
-                this.current = '';
-                this.operatorClicked = false;
-              }
-              this.current = `${this.current}${number}`;
-         },
-    
-         addOperator(operator) {
-              this.operatorClicked = true;
-              this.previous.push(this.current);
-              this.operator.push(operator);
-         },
-    
-         calculate() {
-              let result;
-              const prev = parseFloat(this.previous);
-              const current = parseFloat(this.current);
-              const operator = this.operator;
-    
-              if(isNaN(prev) || isNaN(current)) return;
-    
-              switch(operator) {
-                case '+':
-                     result = prev + current;
-                     break;
-                 case '-':
-                      result = prev - current;
-                      break;
-                 case '*':
-                      result = prev * current;
-                      break;
-                 case '/':
-                      result = prev / current;
-                      break;
-                 default:
-                      return;
-              }
-    
-              this.current = result;
-              this.operator = undefined;
-              this.previous = '';
-         }
-   }
-
+     methods: {
+          clear() {
+               this.current = '0';
+          },
+          sign() {
+               this.current = this.current.charAt(0) === '-' ? this.current.slice(1) : `-${this.current}`;
+          },
+          percent () {
+               this.current = `${parseFloat(this.current) / 100}`;
+          },
+          divide() {
+               this.operator = '/';
+               this.current += this.operator;
+          },
+          multiply() {
+               this.operator = '*';
+               this.current += this.operator;
+          },
+          subtract() {
+               this.operator = '-';
+               this.current += this.operator;
+          },
+          add() {
+               this.operator = '+';
+               this.current += this.operator;
+          },
+          dot() {
+               this.current += '.';
+          },
+          equal() {
+               this.current = eval(this.current);
+          },
+          number(value) {
+               if (this.current === '0') {
+                    this.current = '';
+               }
+               this.current += value;
+          },
+     },
 });
 
-app.mount('#calculatorapp');
+app.mount('#app');
