3858d1487093f8401c81de7625a11f4ce475f063
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2023-12-01 08:49:09 -0600

    commented out set previous()

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const app = Vue.createApp({
 
      data() {
           return {
-               previous: null,
+               // previous: null,
                current: '',
                operator: null,
                operatorClicked: false,
           }
      },
-
+   // Not sure why co-pilot used setPrevious() method, but it's not needed. I commented it out.Going to dig it more
      methods: {
           clear() {
                this.current = '';
@@ -30,27 +30,27 @@ const app = Vue.createApp({
           },
           percent () {
                this.current = `${parseFloat(this.current) / 100}`;
-               this.setPrevious();
+               // this.setPrevious();
           },
           divide() {
                this.operator = '/';
                this.current += this.operator;
-               this.setPrevious();
+               // this.setPrevious();
           },
           multiply() {
                this.operator = '*';
                this.current += this.operator;
-               this.setPrevious();
+               // this.setPrevious();
           },
           substract() {
                this.operator = '-';
                this.current += this.operator;
-               this.setPrevious();
+               // this.setPrevious();
           },
           add() {
                this.operator = '+';
                this.current += this.operator;
-               this.setPrevious();
+               // this.setPrevious();
           },
           dot() {
                this.current += '.';
@@ -59,7 +59,7 @@ const app = Vue.createApp({
          
           equal() {
                this.current = eval(this.current);
-               this.previous = null;
+               // this.previous = null;
               
          
           },
